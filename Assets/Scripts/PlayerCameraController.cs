b6498356de31a2d8de3b3ab466683f7c026aae8a
JOHNSON Ryan G <j015271e@student.staffs.ac.uk>
2018-03-16 21:13:17 +0000

    First Commit

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    GameObject CHARACTER;
+
+    Vector2 MOUSELOOK;
+    Vector2 SMOOTH;
+
+    public float SENSITIVITY = 5.0f;
+    public float SMOOTHING = 2.0f;
+
+	void Start ()
+    {
+        CHARACTER = transform.parent.gameObject;
+    }
+	
+	void Update ()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(SENSITIVITY * SMOOTHING, SENSITIVITY * SMOOTHING));
+
+        SMOOTH.x = Mathf.Lerp(SMOOTH.x, mouseDelta.x, 1f / SMOOTHING);
+        SMOOTH.y = Mathf.Lerp(SMOOTH.y, mouseDelta.y, 1f / SMOOTHING);
+        MOUSELOOK += SMOOTH;
+
+        MOUSELOOK.y = Mathf.Clamp(MOUSELOOK.y, -90.0f, 90.0f); // Stops the camera from going to far up or down.
+
+        transform.localRotation = Quaternion.AngleAxis(-MOUSELOOK.y, Vector3.right);
+        CHARACTER.transform.localRotation = Quaternion.AngleAxis(MOUSELOOK.x, CHARACTER.transform.up);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float SPEED = 15.0f;
+    float SHIFTSPEED = 100.0f;
+
+    void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	void Update ()
+    {
+        float speed = SPEED;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = SHIFTSPEED;
+        }
+
+
+        float dtime = Time.deltaTime;
+        float translation = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+
+        translation *= dtime;
+        strafe *= dtime;
+
+        transform.Translate(strafe, 0, translation);
+
+        if (Input.GetKeyDown("e")) //remove maybe?
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+}
