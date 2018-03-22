7e928ad12cdd2dd707843c60b5e112a8151d1aad
JOHNSON Ryan G <j015271e@student.staffs.ac.uk>
2018-03-22 20:52:44 +0000

    Current having a issue with rotation. Finished AI code and added falling off terrain handler.

--- Assets/Scripts/AIContoller.cs
+++ Assets/Scripts/AIContoller.cs
@@ -2,27 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public struct Actions
+{
+    public bool forward;
+    public bool backwards;
+    public bool left;
+    public bool right;
+    public bool rotateLeft;
+    public bool rotateRight;
+
+    public Actions(bool defaults)
+    {
+        forward = defaults;
+        backwards = defaults;
+        left = defaults;
+        right = defaults;
+        rotateLeft = defaults;
+        rotateRight = defaults;
+    }
+}
+
 public class AIContoller : MonoBehaviour
 {
-    bool ISAIENABLED = true;
+
+    bool IS_AI_ENABLED = true;
     float SPEED = 15.0f;
     private float DELTATIME;
+    Actions ACTIONS;
+
+    float TIME_BETWEEN_MOVEMENT;
+    float LAST_MOVEMENT_DECISION;
+    float TIME_BETWEEN_ROTATION;
+    float LAST_ROTATION_DECISION;
+
+    float ROTATE_LEFT_SPEED;
+    float ROTATE_RIGHT_SPEED;
 
     void Start ()
     {
-		
-	}
+        ACTIONS = new Actions(false);
+    }
 	
 	void Update ()
     {
-        DELTATIME = Time.deltaTime;
-        //StateMachine();
+        if (IS_AI_ENABLED)
+        {
+            DELTATIME = Time.deltaTime;
 
-    }
-
-    void StateMachine()
-    {
-        Turn(50);
+            ACTIONS = ActionDecider(ACTIONS);
+            ActionProcesser(ACTIONS);
+        }
     }
 
     /// <summary>
@@ -56,21 +86,116 @@ public class AIContoller : MonoBehaviour
     }
 
     /// <summary>
-    /// - == Left | + == Right
+    /// -1 == Left | +1 == Right
     /// </summary>
     /// <param name="rotationAmount"></param>
-    void Turn(float rotationAmount) 
+    void Rotate(float rotationAmount) 
     {
         transform.Rotate(Vector3.up, rotationAmount * DELTATIME);
     }
 
-    float RotationAmountRNG()
+    float RNGRotationSpeed()
     {
-        return Random.Range(-500.0f, 500.0f);
+        return Random.Range(10.0f, 300.0f);
     }
 
-    float StateTimeAmountRNG()
+    float RNGActionTime()
     {
         return Random.Range(0.3f, 4.0f);
+    }
+    
+    float RNGRotationTime()
+    {
+        return Random.Range(0.5f, 1.0f);
+    }
+
+    Actions ActionDecider(Actions actions)
+    {
+
+        if (LAST_MOVEMENT_DECISION + TIME_BETWEEN_MOVEMENT <= Time.time)
+        {
+            LAST_MOVEMENT_DECISION = Time.time;
+            TIME_BETWEEN_MOVEMENT = RNGActionTime();
+
+            if (Chance(80))
+            {
+                actions.forward = true;
+            }
+            else
+            {
+                actions.forward = false;
+            }
+        }
+
+        if (LAST_ROTATION_DECISION + TIME_BETWEEN_ROTATION <= Time.time)
+        {
+            LAST_ROTATION_DECISION = Time.time;
+            TIME_BETWEEN_ROTATION = RNGActionTime();
+
+            if (Chance(30))
+            {
+                if (Chance(50))
+                {
+                    actions.rotateRight = false;
+                    actions.rotateLeft = true;
+                    ROTATE_LEFT_SPEED = RNGRotationSpeed();
+                }
+                else
+                {
+                    actions.rotateLeft = false;
+                    actions.rotateRight = true;
+                    ROTATE_RIGHT_SPEED = RNGRotationSpeed();
+                }
+            }
+            else
+            {
+                actions.rotateLeft = false;
+                actions.rotateRight = false;
+            }
+        }
+
+        return actions;
+    }
+
+    bool Chance(float chance)
+    {
+        int num = Random.Range(1, 100);
+
+        if (num <= chance)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void ActionProcesser(Actions actions)
+    {
+        if (actions.forward)
+        {
+            Heading(1);
+        }
+        if (actions.backwards)
+        {
+            Heading(-1);
+        }
+        if (actions.left)
+        {
+            Strafing(-1);
+        }
+        if (actions.right)
+        {
+            Strafing(1);
+        }
+        if (actions.rotateLeft)
+        {
+            Rotate(-ROTATE_LEFT_SPEED);
+        }
+        if (actions.rotateRight)
+        {
+            Rotate(ROTATE_RIGHT_SPEED);
+        }
     }
 }
---
+++ Assets/Scripts/FallingOfTerrainHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingOfTerrainHandler : MonoBehaviour
+{
+    public GameObject ObjectToSpawnOn;
+    public float FallDistance = -10;
+
+    Vector3 ObjectToSpawnOnSize;
+    Vector3 ObjectToSpawnOnPosition;
+    Vector3 Size;
+
+    void Start ()
+    {
+        if (ObjectToSpawnOn == null)
+        {
+            ObjectToSpawnOn = GameObject.Find("Teleport Cube");
+        }
+
+        ObjectToSpawnOnSize = ObjectToSpawnOn.GetComponent<BoxCollider>().bounds.size;
+        ObjectToSpawnOnPosition = ObjectToSpawnOn.GetComponent<Transform>().position;
+        Size = GetComponent<BoxCollider>().bounds.size;
+    }
+	
+	void Update ()
+    {
+        if (transform.position.y < FallDistance)
+        {
+            float y = (ObjectToSpawnOnSize.y / 2) + (Size.y / 2);
+
+            transform.position = new Vector3(ObjectToSpawnOnPosition.x, y, ObjectToSpawnOnPosition.z);
+        }
+	}
+}
