201a62d09be51ed41eb6233abe650e98ddb5f14c
MegaRGJ <ryangjohnson5@gmail.com>
2018-03-30 10:25:56 +0100

    Fixed PlayerCameraController bug where the AI would not rotate the charactor model due to camera controls being enabled. Now uses IF statement to check if ai is enabled.

--- Assets/Scripts/PlayerCameraController.cs
+++ Assets/Scripts/PlayerCameraController.cs
@@ -19,16 +19,19 @@ public class PlayerCameraController : MonoBehaviour
 	
 	void Update ()
     {
-        Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(SENSITIVITY * SMOOTHING, SENSITIVITY * SMOOTHING));
+        if (!GetComponentInParent<AIContoller>().IS_AI_ENABLED)
+        {
+            Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            mouseDelta = Vector2.Scale(mouseDelta, new Vector2(SENSITIVITY * SMOOTHING, SENSITIVITY * SMOOTHING));
 
-        SMOOTH.x = Mathf.Lerp(SMOOTH.x, mouseDelta.x, 1f / SMOOTHING);
-        SMOOTH.y = Mathf.Lerp(SMOOTH.y, mouseDelta.y, 1f / SMOOTHING);
-        MOUSELOOK += SMOOTH;
+            SMOOTH.x = Mathf.Lerp(SMOOTH.x, mouseDelta.x, 1f / SMOOTHING);
+            SMOOTH.y = Mathf.Lerp(SMOOTH.y, mouseDelta.y, 1f / SMOOTHING);
+            MOUSELOOK += SMOOTH;
 
-        MOUSELOOK.y = Mathf.Clamp(MOUSELOOK.y, -90.0f, 90.0f); // Stops the camera from going to far up or down.
+            MOUSELOOK.y = Mathf.Clamp(MOUSELOOK.y, -90.0f, 90.0f); // Stops the camera from going to far up or down.
 
-        transform.localRotation = Quaternion.AngleAxis(-MOUSELOOK.y, Vector3.right);
-        CHARACTER.transform.localRotation = Quaternion.AngleAxis(MOUSELOOK.x, CHARACTER.transform.up);
+            transform.localRotation = Quaternion.AngleAxis(-MOUSELOOK.y, Vector3.right);
+            CHARACTER.transform.localRotation = Quaternion.AngleAxis(MOUSELOOK.x, CHARACTER.transform.up);
+        }
     }
 }
