b1b062f7b418acaf022eb72cf98358a31d0c1b75
MegaRGJ <ryangjohnson5@gmail.com>
2018-03-30 13:19:39 +0100

    Added the Serialisation class which is partly moved over functions form the MultiplayerManager. Also added DeserialiseServerPacket functions which is the first code implemention of the server packet.

--- Assets/Scripts/MultiplayerManager.cs
+++ Assets/Scripts/MultiplayerManager.cs
@@ -6,102 +6,83 @@ using System.Collections.Generic;
 
 public class MultiplayerManager : MonoBehaviour 
 {
-    private int POSITION_ID = 0;
-    private int CONNECT_ID = 1;
-    private int DISCONNECT_ID = 2;
-
     private Communication COMMS;
+    private Serialisation SERIAL;
+
     private Thread NETWORKSERVER_THREAD;
     public string IP_ADDRESS = "127.0.0.1";
     public int PORT = 4739;
     public int MS_INTERVAL = 16; // 1000/ 60 = 16.6666666667
+    public string USERNAME = "Mega";
 
     public GameObject PLAYER_CHARACTER;
     private Transform PLAYER_TRANSFORM;
+    private List<byte[]> PACKET_LIST = new List<byte[]>();
+    private double LAST_SEND = 0;
 
     void Start ()
     {
         PLAYER_TRANSFORM = PLAYER_CHARACTER.GetComponent<Transform>();
         COMMS = new Communication();
-        StartConnection();
+        SERIAL = new Serialisation();
 
-        //UpdateServerWithClientData();
+        StartConnection();
     }
 	
 	void Update ()
     {
-        UpdateServerWithClientData();
+        if (LAST_SEND + MS_INTERVAL <= GetMSTime())
+        {
+            LAST_SEND = GetMSTime();
+
+            UpdateClientWithServerData(); // Check in here if packets are missing?
+
+            UpdateServerWithClientData();
+        }
     }
 
     private void StartConnection()
     {
         COMMS.Connect(IP_ADDRESS, PORT);
+        COMMS.StartDataReceive();
+
         //Send Connect Packet
+        //SendConnectRequest(USERNAME);
     }
 
-    List<byte[]> packetList = new List<byte[]>();
-    double LastSend = 0;
+    public void SendConnectRequest(string name)
+    {
+        PACKET_LIST.Add(SERIAL.SerialiseConnectData(name));
+    }
 
     private void UpdateServerWithClientData()
     {
-        if (LastSend + MS_INTERVAL <= GetMSTime())
+        //Get required data
+        float x = PLAYER_TRANSFORM.position.x;
+        float y = PLAYER_TRANSFORM.position.y;
+        float z = PLAYER_TRANSFORM.position.z;
+        float r = PLAYER_TRANSFORM.rotation.y;
+        
+        //Create any byte packets that needs sending.
+        if (true) //Has connection been achieved?
         {
-            LastSend = GetMSTime();
-
-            //Get required data
-            float x = this.PLAYER_TRANSFORM.position.x;
-            float y = this.PLAYER_TRANSFORM.position.y;
-            float z = this.PLAYER_TRANSFORM.position.z;
-            float r = this.PLAYER_TRANSFORM.rotation.y;
-
-            //Create any byte packets that needs sending.
-            packetList.Add(CreatePositionData(x, y, z, r));
-
-            //Send Packets
-            COMMS.SendData(packetList);
-            packetList.Clear();
+            PACKET_LIST.Add(SERIAL.SerialisePositionData(x, y, z, r));
         }
+        
+        //Send Packets
+        COMMS.SendData(PACKET_LIST);
+        PACKET_LIST.Clear();
     }
-
-
 
     private double GetMSTime()
     {
         return (DateTime.Now - DateTime.MinValue).TotalMilliseconds;
     }
 
-    byte[] CreateConnectData(string username)
+    private void UpdateClientWithServerData()
     {
-        List<byte> byteList = new List<byte>();
+        List<byte[]> packets = COMMS.GetData();
 
-        byteList.AddRange(BitConverter.GetBytes(CONNECT_ID)); //4 bytes
-        byteList.AddRange(System.Text.ASCIIEncoding.ASCII.GetBytes(username));
-
-        return byteList.ToArray();
-    }
-
-    byte[] CreateDisconnecetData(string username)
-    {
-        List<byte> byteList = new List<byte>();
-
-        byteList.AddRange(BitConverter.GetBytes(DISCONNECT_ID)); //4 bytes
-        byteList.AddRange(System.Text.ASCIIEncoding.ASCII.GetBytes(username));
-
-        return byteList.ToArray();
-    }
-
-    byte[] CreatePositionData(float x, float y, float z, float r)
-    {
-        List<byte> byteList = new List<byte>();
-
-        byteList.AddRange(BitConverter.GetBytes(POSITION_ID)); //4 bytes
-        byteList.AddRange(BitConverter.GetBytes(x));
-        byteList.AddRange(BitConverter.GetBytes(y));
-        byteList.AddRange(BitConverter.GetBytes(z));
-        byteList.AddRange(BitConverter.GetBytes(r));
-        //byteList.AddRange(System.Text.ASCIIEncoding.ASCII.GetBytes(alphabet));
-
-        return byteList.ToArray();
-
+        //SERIAL.DeserialiseServerPacket();
     }
 }
---
+++ Assets/Scripts/Serialisation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+public struct ServerPacket
+{
+    public int PlayerID;
+    public float X;
+    public float Y;
+    public float Z;
+    public float Rotation;
+}
+
+public class Serialisation
+{
+    private int POSITION_ID = 0;
+    private int CONNECT_ID = 1;
+    private int DISCONNECT_ID = 2;
+
+    public byte[] SerialiseConnectData(string username)
+    {
+        List<byte> byteList = new List<byte>();
+
+        byteList.AddRange(BitConverter.GetBytes(CONNECT_ID)); //4 bytes
+        byteList.AddRange(System.Text.ASCIIEncoding.ASCII.GetBytes(username));
+
+        return byteList.ToArray();
+    }
+
+    public byte[] SerialiseDisconnecetData(string username)
+    {
+        List<byte> byteList = new List<byte>();
+
+        byteList.AddRange(BitConverter.GetBytes(DISCONNECT_ID)); //4 bytes
+        byteList.AddRange(System.Text.ASCIIEncoding.ASCII.GetBytes(username));
+
+        return byteList.ToArray();
+    }
+
+    public byte[] SerialisePositionData (float x, float y, float z, float r)
+    {
+        List<byte> byteList = new List<byte>();
+
+        byteList.AddRange(BitConverter.GetBytes(POSITION_ID)); //4 bytes
+        byteList.AddRange(BitConverter.GetBytes(x));
+        byteList.AddRange(BitConverter.GetBytes(y));
+        byteList.AddRange(BitConverter.GetBytes(z));
+        byteList.AddRange(BitConverter.GetBytes(r));
+        //byteList.AddRange(System.Text.ASCIIEncoding.ASCII.GetBytes(alphabet));
+
+        return byteList.ToArray();
+
+    }
+
+    public object DeserialiseServerPacket(byte[] packet)
+    {
+        ServerPacket p = new ServerPacket();
+
+        p.PlayerID = BitConverter.ToInt32(packet, 0);
+        p.X = BitConverter.ToSingle(packet, 4);
+        p.Y = BitConverter.ToSingle(packet, 8);
+        p.Z = BitConverter.ToSingle(packet, 12);
+        p.Rotation = BitConverter.ToSingle(packet, 16);
+
+        return p;
+    }
+}
